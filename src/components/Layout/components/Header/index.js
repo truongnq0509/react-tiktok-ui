1e93d020a0dee0561c284246070f45cdd7c4f073
TruongNguyen <truongmoba157@gmail.com>
2022-11-07 10:32:41 +0700

    [#2] Xây dựng UI phần Header #2

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,12 +1,39 @@
 import classNames from 'classnames/bind';
 import styles from './Header.module.scss'
+import images from '~/assets/images'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircleXmark, faMagnifyingGlass, faSpinner } from '@fortawesome/free-solid-svg-icons';
+
 
 const cx = classNames.bind(styles)
+
 
 function Header() {
 	return <header className={cx('wrapper')}>
 		<div className={cx('inner')}>
+			<img
+				src={images.logo}
+				alt="TikTok"
+			/>
+			<div className={cx('search')}>
+				<input
+					placeholder='Seacrch accounts and videos'
+					spellCheck={false}
+				/>
+				<button className={cx('clear')}>
+					{/*Close*/}
+					<FontAwesomeIcon icon={faCircleXmark} />
+				</button>
+				{/* Loading */}
+				<FontAwesomeIcon className={cx('loading')} icon={faSpinner} />
+				<button className={cx('search-btn')}>
+					{/* Search */}
+					<FontAwesomeIcon icon={faMagnifyingGlass} />
+				</button>
+			</div>
+			<div className={cx('action')}>
 
+			</div>
 		</div>
 	</header>
 }

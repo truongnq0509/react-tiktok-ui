c0fb4d7dc622e73496717b39654b77e9b255e19a
TruongNguyen <truongmoba157@gmail.com>
2022-11-08 15:31:18 +0700

    [#4] Xây dựng UI phần Header #4

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,17 +1,52 @@
 import { useState, useEffect } from 'react'
 import classNames from 'classnames/bind';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCircleXmark, faMagnifyingGlass, faSign, faSpinner } from '@fortawesome/free-solid-svg-icons';
-import Tippy from '@tippyjs/react/headless'
+import { faCircleQuestion, faCircleXmark, faCloudUpload, faEarthAmerica, faEllipsisVertical, faKeyboard, faMagnifyingGlass, faMessage, faSign, faSpinner } from '@fortawesome/free-solid-svg-icons';
+import HeadlessTippy from '@tippyjs/react/headless'
+import Tippy from '@tippyjs/react'
+import 'tippy.js/dist/tippy.css'
 
 import styles from './Header.module.scss'
 import images from '~/assets/images'
 import { Wrapper as PopperWrapper } from '~/components/Popper'
 import AccountItem from '~/components/AccountItem';
 import Button from '~/components/Button';
+import Menu from '~/components/Popper/Menu';
 
 
 const cx = classNames.bind(styles)
+
+
+const MENU_ITEMS = [
+	{
+		icon: <FontAwesomeIcon icon={faEarthAmerica} />,
+		title: 'English',
+		children: {
+			title: 'Language',
+			data: [
+				{
+					type: 'language',
+					code: 'en',
+					title: 'English',
+				},
+				{
+					type: 'language',
+					code: 'vi',
+					title: 'Tiếng Việt',
+				}
+			]
+		}
+	},
+	{
+		icon: <FontAwesomeIcon icon={faCircleQuestion} />,
+		title: 'Feedback and Help',
+		to: '/feedback'
+	},
+	{
+		icon: <FontAwesomeIcon icon={faKeyboard} />,
+		title: 'Keyboard shortcuts',
+	},
+]
 
 
 function Header() {
@@ -23,6 +58,19 @@ function Header() {
 		}, 0)
 	}, [])
 
+	// Handle
+	const handleMenuChange = menuItem => {
+		switch (menuItem.type) {
+			case 'language':
+				// Handle change language
+				break;
+			default:
+
+		}
+	}
+
+	const currentUser = true
+
 
 	return <header className={cx('wrapper')}>
 		<div className={cx('inner')}>
@@ -30,7 +78,7 @@ function Header() {
 				src={images.logo}
 				alt="TikTok"
 			/>
-			<Tippy
+			<HeadlessTippy
 				interactive
 				visible={searchResult.length > 0}
 				render={attrs => (
@@ -64,13 +112,48 @@ function Header() {
 						<FontAwesomeIcon icon={faMagnifyingGlass} />
 					</button>
 				</div>
-			</Tippy>
-			<div className={cx('action')}>
-				<Button text> Upload </Button>
-				<Button primary> Log in </Button>
+			</HeadlessTippy>
+
+
+			<div className={cx('actions')}>
+				{currentUser ? (
+					<>
+						<Tippy
+							content="Upload video"
+							placement='bottom'
+							trigger='click'
+						>
+							<button className={cx('action-btn')}>
+								<FontAwesomeIcon icon={faCloudUpload} />
+							</button>
+						</Tippy>
+					</>
+				) : (
+					<>
+						<Button text> Upload </Button>
+						<Button primary> Log in </Button>
+
+					</>
+				)
+				}
+				<Menu items={MENU_ITEMS} onChange={handleMenuChange} >
+					{currentUser ? (
+						<img
+							src="https://p9-sign-sg.tiktokcdn.com/aweme/720x720/tos-alisg-avt-0068/884d6d4018b9b9b050e8069e772e0009.jpeg?x-expires=1668067200&x-signature=%2FS%2FW5TUqfwfMi%2BIU5kChkSNhvpQ%3D"
+							className={cx('avatar')}
+							alt="Tran Thi Thao"
+							style={{ width: '50px', height: '50px' }}
+						/>
+					) : (
+						<button className={cx('more-btn')}>
+							<FontAwesomeIcon icon={faEllipsisVertical} />
+						</button>
+					)}
+				</Menu>
 			</div>
-		</div>
-	</header>
+
+		</div >
+	</header >
 }
 
 export default Header;
---
+++ src/components/Popper/Menu/Header.js
@@ -0,0 +1,21 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faAngleDoubleLeft, faAngleLeft, faChevronCircleLeft } from "@fortawesome/free-solid-svg-icons";
+import classNames from "classnames/bind";
+import styles from './Menu.module.scss';
+
+const cx = classNames.bind(styles)
+
+function Header({ title, onBack }) {
+	return (
+		<header className={cx('header')}>
+			<button className={cx('back-btn')} onClick={onBack}>
+				<FontAwesomeIcon icon={faAngleLeft} />
+			</button>
+			<h4 className={cx('header-title')}>
+				{title}
+			</h4>
+		</header>
+	);
+}
+
+export default Header;
